923dd2dce6787ffbbb876397bc6e0555858d88d1
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2022-03-23 14:13:57 -0600

    testAddFoodItem
    here is the first test I wrote just wanted to make sure that it was correct with you guys before I write more

--- Testing/UnitTestHamper.java
+++ Testing/UnitTestHamper.java
@@ -1,6 +1,23 @@
 import org.junit.*;
 import static org.unit.Assert.*;
+import java.util.ArrayList;
 
 public class UnitTestHamper{
 
+    HamperTest(){}
+
+    @Test
+    public void testAddFoodItem(){
+        int[] members = {1, 2};
+        FoodItem orange = new FoodItem("orange", 1234, 10, 14, 35, 10, 150);
+        Family family = new Family(members, 1);
+        Hamper hamper = new Hamper(family);
+
+        System.out.println("addFoodItem");
+        hamper.addFoodItem(orange);
+        ArrayList <FoodItem> array = hamper.getFoodList();
+        assertEquals("Food item insertion was incorrect: ", orange, array.get(0));
+    }
+
 }
+
