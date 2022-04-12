6128f740fef1d9b18376b44fecc1733b97bb6d3e
afrerichs <101382243+afrerichs@users.noreply.github.com>
2022-04-12 15:26:31 -0600

    Update ChildUnder8.java

--- code/ChildUnder8.java
+++ code/ChildUnder8.java
@@ -3,7 +3,7 @@ package edu.ucalgary.ensf409;
 public class ChildUnder8 extends Client{
     private static int CLIENT_ID = 4;
 
-    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
+    public ChildUnder8(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
