6b1212e3a3846c77f1dc6319922424485c814ba3
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 15:59:59 -0600

    Update ChildOver8.java

--- code/ChildOver8.java
+++ code/ChildOver8.java
@@ -1,14 +1,15 @@
 package edu.ucalgary.ensf409;
 
 public class ChildOver8 extends Client{
-    private static int CLIENT_ID = 3;
+    private static final int CLIENT_ID = 3;
 
-    public ChildOver8(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
+    //Constructor for the ChildOver8 class
+    public ChildOver8(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories) throws IllegalArgumentException{
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
     @Override
-    public int getClientID(){
+    public int getClientID(){ //Getter for the CLIENT_ID
         return CLIENT_ID;
     }
 }
