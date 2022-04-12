9cf51862dde381bd0c38e16d78c1ea313a0f1d53
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2022-04-12 15:35:29 -0600

    Create FoodShortageException.java

---
+++ code/FoodShortageException.java
@@ -0,0 +1,9 @@
+package edu.ucalgary.ensf409;
+
+import java.lang.Exception;
+
+public class FoodShortageException {
+    public FoodShortageException(){
+        super("Food Shortage Exception");
+    }
+}
