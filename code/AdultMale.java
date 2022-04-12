4a204e3ace10e987af57402dd3cde9a0c975b469
afrerichs <101382243+afrerichs@users.noreply.github.com>
2022-04-12 15:26:00 -0600

    Update AdultMale.java

--- code/AdultMale.java
+++ code/AdultMale.java
@@ -3,7 +3,7 @@ package edu.ucalgary.ensf409;
 public class AdultMale extends Client{
     private static int CLIENT_ID = 1;
 
-    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
+    public AdultMale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
