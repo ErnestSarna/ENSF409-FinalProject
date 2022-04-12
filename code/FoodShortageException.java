4952501f4751ee81b11e248d858165e2502f1218
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 16:55:47 -0600

    Update FoodShortageException.java

--- code/FoodShortageException.java
+++ code/FoodShortageException.java
@@ -2,6 +2,7 @@ package edu.ucalgary.ensf409;
 
 import java.lang.Exception;
 
+//Custom expection that is thrown when there are not enough food items in the database to fill a required hamper
 public class FoodShortageException extends Exception{
     public FoodShortageException(){
         super("Food Shortage Exception");
