e55009f6089e5cb19e052be0a0817dfab38ed3c3
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 14:49:58 -0600

    Update ChildUnder8.java

--- code/ChildUnder8.java
+++ code/ChildUnder8.java
@@ -3,7 +3,7 @@ package edu.ucalgary.ensf409;
 public class ChildUnder8 extends Client{
     private static int CLIENT_ID = 4;
 
-    public ChildUnder8(double grainsPercent, double proteinsPercent, double fruitsVeggiesPercent, double otherPercent, int calories){
+    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
