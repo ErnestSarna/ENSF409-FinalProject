703e0908d30b992ae5060dbb3f66e054e9d0f08c
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2022-04-12 15:42:18 -0600

    Update FoodShortageException.java

--- code/FoodShortageException.java
+++ code/FoodShortageException.java
@@ -2,7 +2,7 @@ package edu.ucalgary.ensf409;
 
 import java.lang.Exception;
 
-public class FoodShortageException {
+public class FoodShortageException extends Exception{
     public FoodShortageException(){
         super("Food Shortage Exception");
     }
