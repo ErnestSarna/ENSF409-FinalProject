c0cb835447b18acb248f3747d878ae07965fa270
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 16:07:10 -0600

    Update AdultFemale.java

--- code/AdultFemale.java
+++ code/AdultFemale.java
@@ -1,14 +1,19 @@
 package edu.ucalgary.ensf409;
 
 public class AdultFemale extends Client{
-    private static int CLIENT_ID = 2;
+    private static final int CLIENT_ID = 2;
 
-    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
+    //Constructor for the AdultFemale class
+    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories) throws IllegalArgumentException{
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
+    public AdultFemale(){ //An empty constructor for the AdultFemale class
+        super();
+    }
+
     @Override
-    public int getClientID(){
+    public int getClientID(){ //Getter for the CLIENT_ID
         return CLIENT_ID;
     }
 }
