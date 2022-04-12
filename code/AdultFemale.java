96e96ca2b1f9dff72fbafe34a62f95a1accac227
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 14:49:08 -0600

    Update AdultFemale.java

--- code/AdultFemale.java
+++ code/AdultFemale.java
@@ -3,7 +3,7 @@ package edu.ucalgary.ensf409;
 public class AdultFemale extends Client{
     private static int CLIENT_ID = 2;
 
-    public AdultFemale(double grainsPercent, double proteinsPercent, double fruitsVeggiesPercent, double otherPercent, int calories){
+    public AdultFemale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
