f8ce261467ba2b5c45a5848120de00c5777b7c65
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-12 16:07:54 -0600

    Update ChildUnder8.java

--- code/ChildUnder8.java
+++ code/ChildUnder8.java
@@ -1,14 +1,19 @@
 package edu.ucalgary.ensf409;
 
 public class ChildUnder8 extends Client{
-    private static int CLIENT_ID = 4;
+    private static final int CLIENT_ID = 4;
 
-    public ChildUnder8(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories){
+    //Constructor for the ChildUnder8 class
+    public ChildUnder8(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories) throws IllegalArgumentException{
         super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
     }
 
+    public ChildUnder8(){ //An empty constructor for the ChildUnder8 class
+        super();
+    }
+
     @Override
-    public int getClientID(){
+    public int getClientID(){ //Getter for the CLIENT_ID
         return CLIENT_ID;
     }
 }
