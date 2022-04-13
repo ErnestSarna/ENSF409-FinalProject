7cd066c6dfda6afbbda2486d98e4cf60c060f415
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2022-04-13 12:51:56 -0600

    added comments

--- code/FoodEfficiencyAlgorithm.java
+++ code/FoodEfficiencyAlgorithm.java
@@ -41,6 +41,7 @@ public class FoodEfficiencyAlgorithm{
         
         ArrayList<FoodItem> current = validCombinations.get(0);
         int neededCalories = (int) hamper.getFamily().getNeededCalories();
+	//Finds most efficient and valid food hamper
         for(int i=1;i<validCombinations.size();i++){
 
             int calories1 = 0;
@@ -59,6 +60,7 @@ public class FoodEfficiencyAlgorithm{
             }
         }
         ArrayList<FoodItem> foodList = current;
+	//deletes food items from foodlist
         for(int i = 0; i < foodList.size(); i++){
             int id = foodList.get(i).getID();
             for(int j = 0; j < foodItems.size(); j++){
@@ -71,6 +73,8 @@ public class FoodEfficiencyAlgorithm{
         hamper.addFoodList(foodList);
     }
     
+    //Finds all subsets of an array
+    //Code adapted from: https://www.geeksforgeeks.org/recursive-program-to-generate-power-set/
     public void powerSet(int index, ArrayList<FoodItem> curr){
         int n = foodItems.size();
  
@@ -101,51 +105,11 @@ public class FoodEfficiencyAlgorithm{
 	curr.add(foodItems.get(index));
         powerSet(index + 1, curr);
      }
-    
+    //updates fooditems in the database using food id
     public void updateInventory(){
         for(int i = 0; i < hamper.getItems(); i++){
             int id = hamper.getFoodList().get(i).getID();
             dataBase.deleteItem(id);
         }
     }    
-    /* 
-    private void findCombinations(int n, int r){
-        // A temporary array to store all combination one by one
-        FoodItem data[] = new FoodItem[r];
- 
-        // Print all combination using temporary array 'data[]'
-        combinationUtil(n, r, 0, data, 0);
-    }
-
-    private void combinationUtil(int n, int r, int index, FoodItem data[], int j){
-        // Current combination is ready to be printed, print it
-        if (index == r){
-            double fruitsVeg = 0, grains = 0, proteins = 0, other = 0;
-            for(int i=0; i<data.length; i++){
-                fruitsVeg += data[i].getTotalFruitsVeg();
-                grains += data[i].getTotalGrains();
-                proteins += data[i].getTotalProtein();
-                other += data[i].getTotalOther();
-            }
-            if(fruitsVeg >= hamper.getFamily().getNeededFV() && grains >= hamper.getFamily().getNeededGrains() && 
-                proteins >= hamper.getFamily().getNeededProtein() && other >= hamper.getFamily().getNeededOther()){
-                    validCombinations.add(data);
-            }
-            return;
-        }
- 
-        // When no more elements are there to put in data[]
-        if (j >= n){
-            return;
-        }
-
-        // current is included, put next at next location
-        data[index] = foodItems.get(j);
-
-        combinationUtil(n, r, index+1, data, j+1);
- 
-        // current is excluded, replace it with next (Note that
-        // i+1 is passed, but index is not changed)
-        combinationUtil(n, r, index, data, j+1);
-    } */
 }
