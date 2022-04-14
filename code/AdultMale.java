c1329de6f34b22b6c4d82a513f07ae842c57d8b3
Ernest Sarna <101298308+Erners@users.noreply.github.com>
2022-04-14 14:21:23 -0600

    Update AdultMale.java

--- code/AdultMale.java
+++ code/AdultMale.java
@@ -1,19 +1,75 @@
 package edu.ucalgary.ensf409;
 
+/*
+@author     Robert Hauta   <a href="mailto:robert.hauta@ucalgary.ca">
+@author     Joshua Weir    <a href="mailto:joshua.weir@ucalgary.ca">
+@author     Ernest Sarna   <a href="mailto:ernest.sarna@ucalgary.ca">
+@auhor      Aaron Frerichs <a href="mailto:aaron.frerichs@ucalgary.ca">
+@version    1.4
+@since      1.0
+*/
+
+/*
+This class holds all the information for the Adult Male client type.
+*/
+
 public class AdultMale extends Client{
     private static final int CLIENT_ID = 1;
+    private static double wholeGrains;
+    private static double proteins;
+    private static double fruitsVeggies;
+    private static double other;
+    private static int calories;
 
-    //Constructor for the AdultMale class
-    public AdultMale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int calories) throws IllegalArgumentException{
-        super(grainsPercent, proteinsPercent, fruitsVeggiesPercent, otherPercent, calories);
+    //Constructor for the AdultMale class that sets all the variables, it uses the percentageToValue method to convert the different food group
+    //percentages into actual numbers using the total amount of calories
+    public AdultMale(int grainsPercent, int proteinsPercent, int fruitsVeggiesPercent, int otherPercent, int totalCalories) throws IllegalArgumentException{
+        if(grainsPercent < 0 || proteinsPercent < 0 || fruitsVeggiesPercent < 0 
+            || otherPercent < 0 || totalCalories < 0){
+            throw new IllegalArgumentException();
+        }
+
+        calories = totalCalories;
+        wholeGrains = percentageToValue(grainsPercent, calories);
+        proteins = percentageToValue(proteinsPercent, calories);
+        fruitsVeggies = percentageToValue(fruitsVeggiesPercent, calories);
+        other = percentageToValue(otherPercent, calories);
     }
 
     public AdultMale(){ //An empty constructor for the AdultMale class
         super();
     } 
 
+    //Overridden getters for all the variables
+
     @Override
-    public int getClientID(){ //Getter for the CLIENT_ID
+    public int getClientID(){
         return CLIENT_ID;
     }
+
+    @Override
+    public double getWholeGrains() {
+        return wholeGrains;
+    }
+
+    @Override
+    public double getProteins() {
+        return proteins;
+    }
+
+    @Override
+    public double getFruitsVeggies() {
+        return fruitsVeggies;
+    }
+
+    @Override
+    public double getOther() {
+        return other;
+    }
+
+    @Override
+    public int getCalories() {
+        return calories;
+    }
 }
+
