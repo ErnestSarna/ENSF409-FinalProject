8cb99eaa9da48c6269bd28aae867aaa91681ec14
RobertHauta <97628834+RobertHauta@users.noreply.github.com>
2022-04-14 16:16:41 -0600

    Update FoodEfficiencyAlgorithmTest.java

--- Testing/FoodEfficiencyAlgorithmTest.java
+++ Testing/FoodEfficiencyAlgorithmTest.java
@@ -7,7 +7,11 @@ import static org.junit.Assert.*;
 
 
 public class FoodEfficiencyAlgorithmTest{
-
+    /*
+    For all other tests, no testing maybe be done
+    as they are all reliant on database connection which
+    was specified does not need to be tested
+    */
 
     @Test 
     public void testGetHamper(){
@@ -21,35 +25,5 @@ public class FoodEfficiencyAlgorithmTest{
         assertEquals("Get hamper did not return expected result: ",hamper, actual);
     }
 
-    @Test
-    public void testFindContributions(){
-        int[] members = {1, 1,1,1};
-        Family family = new Family(members);
-        Hamper hamper = new Hamper(family);
-        FoodEfficiencyAlgorithm eff = new FoodEfficiencyAlgorithm(hamper);
 
-        System.out.println("testFindContributions");
-        double[] actual = eff.findContributions();
-        assertNotNull("Find Contributions did not return a double array: ", actual);
-        assertEquals("Double array was not of length four: ", 4, actual.length);
-    }
-
-    /* 
-     * Note test for excpetion 
-     * in findContributions cannot be tested
-     * as requires database access
-     */
-
-    @Test
-    public void testCheckEffieciency(){
-        int[] members = {1, 1,1,1};
-        Family family = new Family(members);
-        Hamper hamper = new Hamper(family);
-        FoodEfficiencyAlgorithm eff = new FoodEfficiencyAlgorithm(hamper);
-
-        System.out.println("testCheckEfficiency");
-        int actual = eff.checkEfficiency();
-        int expected = 0;
-        assertEquals("Efficiency did not match expected: ",expected, actual);
-    }
 }
