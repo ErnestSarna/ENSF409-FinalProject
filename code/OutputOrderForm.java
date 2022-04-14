5cb4ddcea809ed8de22a13cbe0d2a61ce549c1ba
afrerichs <101382243+afrerichs@users.noreply.github.com>
2022-04-14 14:39:26 -0600

    Update OutputOrderForm.java

--- code/OutputOrderForm.java
+++ code/OutputOrderForm.java
@@ -22,9 +22,9 @@ class OutputOrderForm{
 			"Date: " + LocalDate.now().toString() + "<br>" +
 			"<br>" +
 			"Original Request: <br>" +
-			originalRequest() + "<br>" + 
+			originalRequestString() + "<br>" + 
 			"<br>" + 
-			hamperItems() + "<html>";
+			hamperItemsString() + "<html>";
 
 		return orderForm;			
 	}
@@ -38,9 +38,9 @@ class OutputOrderForm{
 			"Date: " + LocalDate.now().toString() + "\n" +
 			"\n" +
 			"Original Request: \n" +
-			originalRequest() + "\n" + 
+			originalRequestPrint() + "\n" + 
 			"\n" + 
-			hamperItems() + "\n";
+			hamperItemsPrint() + "\n";
 		
 		try{
 			String fileName = ORDER_FORM.getName() + "'s_Order.txt";
@@ -57,7 +57,8 @@ class OutputOrderForm{
 	}
 	
 	//Helper methods for creating order form string
-	public String hamperItems(){ 
+	
+	public String hamperItemsPrint(){ 
 		ArrayList<Hamper> hampers = ORDER_FORM.getHampers();
 		Iterator<Hamper> iter = hampers.iterator();
 		StringBuilder itemBuilder = new StringBuilder(); //Probably need to set a really large size, leaving it default for now
@@ -82,7 +83,33 @@ class OutputOrderForm{
 		return itemBuilder.toString();
 	}
 	
-	public String originalRequest(){	
+	
+	public String hamperItemsString(){ 
+		ArrayList<Hamper> hampers = ORDER_FORM.getHampers();
+		Iterator<Hamper> iter = hampers.iterator();
+		StringBuilder itemBuilder = new StringBuilder(); //Probably need to set a really large size, leaving it default for now
+		
+		int hamperNum = 1;
+		while(iter.hasNext()){//Iterate through Order to grab each hamper
+			ArrayList<FoodItem> foodList = iter.next().getFoodList();
+			Iterator<FoodItem> foodIter = foodList.iterator();
+			StringBuilder itemList = new StringBuilder(); //Size?
+			
+			
+			while(foodIter.hasNext()){ //Iterate through food list
+				FoodItem currentItem = foodIter.next();
+				itemList.append(currentItem.getID() + "		" + currentItem.getName() + "<br>");
+			}	
+			
+			
+			itemBuilder.append("Hamper " + hamperNum + " Items: <br>" + itemList.toString() + "<br>");
+			hamperNum++;
+				
+		}
+		return itemBuilder.toString();
+	}
+	
+	public String originalRequestPrint(){	
 		ArrayList<Hamper> hampers = ORDER_FORM.getHampers();
 		Iterator<Hamper> iter = hampers.iterator();
 		StringBuilder requestBuilder = new StringBuilder(); //Set size?
@@ -159,6 +186,89 @@ class OutputOrderForm{
 			requestBuilder.append("Hamper " + hamperNum + ": " + familyGetter) ;
 			if(iter.hasNext()){
 				requestBuilder.append("\n");
+			}
+			hamperNum++;
+		}
+		return requestBuilder.toString();
+	}
+	
+	public String originalRequestString(){	
+		ArrayList<Hamper> hampers = ORDER_FORM.getHampers();
+		Iterator<Hamper> iter = hampers.iterator();
+		StringBuilder requestBuilder = new StringBuilder(); //Set size?
+		
+		int hamperNum = 1;
+		while(iter.hasNext()){	//Iterate through Order to grab each hamper
+			ArrayList<Client> familyList = iter.next().getFamily().getFamilyMembers();
+			Iterator<Client> familyIterator = familyList.iterator();
+			
+			StringBuilder familyGetter = new StringBuilder(); //Set size?
+			
+			int numMen = 0;
+			int numWomen = 0;
+			int numChildOverEight = 0;
+			int numChildUnderEight = 0;
+			
+			while(familyIterator.hasNext()){ //Iterate through family to grab each member
+				int famMember = familyIterator.next().getClientID();
+				if(famMember == 1){
+					numMen++;
+				}
+				else if(famMember == 2){
+					numWomen++;
+				}
+				else if(famMember == 3){
+					numChildOverEight++;
+				}
+				else if (famMember == 4){
+					numChildUnderEight++;
+				}
+			}
+			if(numMen != 0){
+				if(numMen == 1){
+					familyGetter.append(numMen + " Adult Male");
+				}
+				else{
+					familyGetter.append(numMen + " Adult Males");
+				}
+				if(numWomen != 0 || numChildOverEight != 0 || numChildUnderEight != 0){
+					familyGetter.append(", ");
+				}
+			}
+			if(numWomen != 0){
+				if(numWomen == 1){
+					familyGetter.append(numWomen + " Adult Female");
+				}
+				else{
+					familyGetter.append(numWomen + " Adult Females");
+				}
+				if(numChildOverEight != 0 || numChildUnderEight != 0){
+					familyGetter.append(", ");
+				}
+			}
+			if(numChildOverEight != 0){
+				if(numChildOverEight == 1){
+					familyGetter.append(numChildOverEight + " Child over 8 ");
+				}
+				else{
+					familyGetter.append(numChildOverEight + " Children over 8");
+				}
+				if(numChildUnderEight != 0){
+					familyGetter.append(", ");
+				}
+			}
+			if(numChildUnderEight != 0){
+				if(numChildUnderEight == 1){
+					familyGetter.append(numChildUnderEight + " Child under 8");
+				}
+				else{
+					familyGetter.append(numChildUnderEight + " Children under 8");
+				}
+			}
+
+			requestBuilder.append("Hamper " + hamperNum + ": " + familyGetter) ;
+			if(iter.hasNext()){
+				requestBuilder.append("<br>");
 			}
 			hamperNum++;
 		}
